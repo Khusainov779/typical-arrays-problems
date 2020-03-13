9ffaaff415497b6c004c1987f7af350c285b529a
Khusainov779 <khusainov779@gmail.com>
2020-03-13 13:28:56 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let minValue = Math.min.apply(null, array);
+    return minValue;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let maxValue = Math.max.apply(null, array);
+    return maxValue;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let sumValue = array.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    return (avgValue = sumValue / array.length);
+};
